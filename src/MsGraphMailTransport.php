7e677cea3bfff5047ba0c87281d00019206c58cc
lsi2021 <100166834+lsi2021@users.noreply.github.com>
2024-04-11 12:07:17 +0200

    Update MsGraphMailTransport.php

--- src/MsGraphMailTransport.php
+++ src/MsGraphMailTransport.php
@@ -99,8 +99,7 @@ class MsGraphMailTransport extends Transport {
         return array_filter([
             'subject' => $message->getSubject(),
             'sender' => $this->toRecipientCollection($from)[0],
-            'from' => $this->toRecipientCollection($from)[0],
-            'returnPath' => $this->toRecipientCollection($message->getReturnPath()),                
+            'from' => $this->toRecipientCollection($from)[0],               
             'replyTo' => $this->toRecipientCollection($message->getReplyTo()),
             'toRecipients' => $this->toRecipientCollection($message->getTo()),
             'ccRecipients' => $this->toRecipientCollection($message->getCc()),
